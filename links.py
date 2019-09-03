979b5fb70809ba8e73a7710ba8720ee918a4c16d
Mattia Ryffel <matt@disneyresearch.com>
2019-09-03 12:09:44 +0200

    added sequential mode

--- links.py
+++ links.py
@@ -2,9 +2,10 @@ import json
 from random import randrange
 from random import choice
 
-class Links:   
 
-    def __init__(self, file_path):        
+class Links:
+
+    def __init__(self, file_path):
         self.last_subreddit = -1
         self.last_link = -1
         self.file_path = file_path
@@ -15,23 +16,32 @@ class Links:
             self.data = json.load(read_file)
 
     def get_link(self, idx):
-        link_type = self.data[idx]["type"]
+        link_type = self.get_type(idx)
+        title = self.get_random_title(idx)
+        content = self.get_random_content(idx)
+        subreddit = self.get_random_subreddit(idx)
+        return link_type, content, title, subreddit
 
-        last_title = -1 if "last_title" not in self.data[idx] else self.data[idx]["last_title"]
-        title_idx = self.random_not(last_title, len(self.data[idx]["titles"]))
-        self.data[idx]["last_title"] = title_idx
-        title = self.data[idx]["titles"][title_idx]
-        
-        last_content = -1 if "last_content" not in self.data[idx] else self.data[idx]["last_content"]
-        content_idx = self.random_not(last_content, len(self.data[idx]["content"]))
-        self.data[idx]["last_content"] = content_idx
-        content = self.data[idx]["content"][content_idx]
+    def get_type(self, idx):
+        return self.data[idx]["type"]
 
+    def get_random_subreddit(self, idx):
         last_subreddit = -1 if "last_subreddit" not in self.data[idx] else self.data[idx]["last_subreddit"]
         subreddit_idx = self.random_not(last_subreddit, len(self.data[idx]["subreddits"]))
         self.data[idx]["last_subreddit"] = subreddit_idx
-        subreddit = self.data[idx]["subreddits"][subreddit_idx]
-        return link_type, content, title, subreddit
+        return self.data[idx]["subreddits"][subreddit_idx]
+
+    def get_random_content(self, idx):
+        last_content = -1 if "last_content" not in self.data[idx] else self.data[idx]["last_content"]
+        content_idx = self.random_not(last_content, len(self.data[idx]["content"]))
+        self.data[idx]["last_content"] = content_idx
+        return self.data[idx]["content"][content_idx]
+
+    def get_random_title(self, idx):
+        last_title = -1 if "last_title" not in self.data[idx] else self.data[idx]["last_title"]
+        title_idx = self.random_not(last_title, len(self.data[idx]["titles"]))
+        self.data[idx]["last_title"] = title_idx
+        return self.data[idx]["titles"][title_idx]
 
     def get_random(self):
         idx = self.random_not(self.last_link, self.len())
--- main.py
+++ main.py
@@ -1,47 +1,84 @@
 import praw
 import time
 import datetime
+import threading
 import links
 import msvcrt
 from enum import IntEnum
 
-#config
-post_delay = datetime.timedelta(minutes=30)
-file_path = "links.json"
 
+# config
+post_delay = datetime.timedelta(minutes=30)
+file_path = "matt.json"
+random_posts = False
+debug = True
+keepGoing = True
 links = links.Links(file_path)
 reddit = praw.Reddit('bot')
 
 print('# Delay: ', post_delay.total_seconds())
 print('# Links: ', links.len())
+print('# Random: ', random_posts)
+print('# Debug: ', debug)
 print('# Enter to stop, L to reload')
 
-keepGoing = True
-while(keepGoing):   
-    
-    #todo put this in a separate thread
-    if msvcrt.kbhit():
-        key = msvcrt.getch().decode('utf-8')        
-        if key == '\r' or key == '\n':
-            break
-        elif key == 'l':
-            print("> reloading links")
-            links.load()
-            
-    #post        
-    link_type, content, title, subreddit = links.get_random()
-    subreddit = reddit.subreddit(subreddit)
 
-    if link_type == "url":
-        subreddit.submit(title, url=content)
-    elif link_type == "image":
-        subreddit.submit_image(title, content, timeout=20)
-    elif link_type == "video":
-        subreddit.submit_video(title, content, timeout=25)
-    else:
-        print("Unknown link_type: ", link_type)
+def thread_check_key():
+    global keepGoing
+    global links
+    while(keepGoing):
+        if msvcrt.kbhit():
+            key = msvcrt.getch().decode('utf-8')        
+            if key == '\r' or key == '\n':
+                keepGoing = False
+                exit()                
+            elif key == 'l':
+                print("> reloading links")
+                links.load()
 
-    print("{} Posting in {} - {}".format(datetime.datetime.now(), subreddit, title))
-    time.sleep(post_delay.total_seconds())
 
-print("> End")
+def post(link_type, content, title, sr):
+        subreddit = reddit.subreddit(sr)
+
+        if not debug:
+            if link_type == "url":
+                print("Unknown link_type: ", link_type)
+                subreddit.submit(title, url=content)
+            elif link_type == "image":
+                print("Unknown link_type: ", link_type)
+                subreddit.submit_image(title, content, timeout=20)
+            elif link_type == "video":
+                subreddit.submit_video(title, content, timeout=25)
+            else:
+                print("Unknown link_type: ", link_type)
+
+        print("{} Posting in {} - {}".format(datetime.datetime.now(), sr, title))
+
+
+def random_post():
+    global keepGoing
+    while(keepGoing):
+        post(links.get_random())
+        time.sleep(post_delay.total_seconds())
+
+
+def sequential_post():
+    global keepGoing
+    for i in range(links.len()):
+        current = links.data[i]
+        for r in links.data[i]["subreddits"]:
+            title = links.get_random_title(i)
+            content = links.get_random_content(i)
+            link_type = links.get_type(i)
+            post(link_type, content, title, r)
+    keepGoing = False
+
+
+# main
+x = threading.Thread(target=thread_check_key)
+x.start()
+
+if random_posts:
+    random_post()
+else:
+    sequential_post()
